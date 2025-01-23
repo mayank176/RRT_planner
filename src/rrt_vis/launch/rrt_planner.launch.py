45771d6c9edbed65d35e73db3a295e8bbc72e245
Mayank Prashar <mayankprashar295@gmail.com>
2025-01-23 01:49:27 -0600

    update node names

--- src/rrt_vis/launch/rrt_planner.launch.py
+++ src/rrt_vis/launch/rrt_planner.launch.py
@@ -11,13 +11,18 @@ def generate_launch_description():
     pkg_dir = get_package_share_directory('rrt_vis')
     config_file = os.path.join(pkg_dir, 'config', 'rrt_params.yaml')
     
-    
+    print(f"Loading config from: {config_file}")
+    with open(config_file, 'r') as f:
+        print("Config file contents:")
+        print(f.read())
+
+
+    print(f"Loading config from: {config_file}")
     rviz_arg = DeclareLaunchArgument(
         'rviz',
         default_value='true',
         description='Start RViz2'
     )
-
 
     rviz_config_path = os.path.expanduser('~/Documents/RRT_planner/src/rrt_vis/rviz/rrt_vis_conf.rviz')
 
@@ -35,14 +40,14 @@ def generate_launch_description():
         Node(
             package='mpc',
             executable='mpc_node',
-            name='mpc_node',
+            name='quadrotor_controller',
             parameters=[config_file]
         ),
         
         Node(
             package='rrt_vis',
             executable='rrt_vis_node',
-            name='rrt_vis_node',
+            name='path_visualizer',
             parameters =[config_file]
         )
     ])
